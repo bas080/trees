f7e6261e1786b65ded0ad1fd8ce8dd6dcf817b49
bas080 <bas080@hotmail.com>
2013-04-30 05:33:58 +0200

    Adde textures

---
+++ aliases.lua
@@ -0,0 +1,4 @@
+minetest.register_alias("moretrees:jungletree_sapling", "trees:jungletree_sapling")
+minetest.register_alias("moretrees:jungletree_leaves_green", "trees:leaves_green")
+minetest.register_alias("moretrees:jungletree_leaves_yellow", "trees:leaves_yellow")
+minetest.register_alias("moretrees:jungletree_leaves_red", "trees:leaves_red")
